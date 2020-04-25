971c73d3ff8e3b37197e51bd30044a5858e0651b
arakilian0 <arakilian0@gmail.com>
2020-04-25 09:34:18 -0700

    UPDATED main function: it's no longer self invoked

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-module.exports = (function(_log=require('@toolio/config')) {
-    if(_log && _log.log && _log.log.type) {
-        let log = _log.log
-        if(Object.keys(log.type).length) {
-            let api = {}
-            for(prop in log.type) {
-                let type = {}
-                type[prop] = _log.log.type[prop]
-                api[prop] = require('./lib/log')(type, _log)
-            }
-            return api
-        } else return console.log('error: no log types are defined')
-    } else return console.log('error: no log types are defined')
-})()
+module.exports = function(log) {
+    let _log; !log ? _log = require('@toolio/config') : _log = log
+    if(_log && _log.log) {
+        if(_log.log.type) {
+            if(Object.keys(_log.log.type).length) {
+                let api = {}
+                for(prop in _log.log.type) {
+                    let type = {}
+                    type[prop] = _log.log.type[prop]
+                    api[prop] = require('./lib/log')(type, _log)
+                }
+                return api
+            } else { console.log('toolio:log:error: no log types are defined'); process.exit() }
+        } else { console.log('toolio:log:error: no log types are defined'); process.exit() }
+    } else { console.log('toolio:log:error: no log object detected'); process.exit() }
+}
--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-const log = require('../index')
+const log = require('../index')()
 
-log.error('this error message will get logged only to the console')
+// log.error('this error message will get logged only to the console')
 log.test('this test message won\'t actually log')
